769502e75d85f02bcade4d3e658ec34d2ac76b6c
braincoin <axelxod@gmail.com>
2016-05-03 07:46:42 +0500

    1.3.2.4

--- src/main.cpp
+++ src/main.cpp
@@ -1543,15 +1543,15 @@ int64_t GetBlockValue(int nBits, int nHeight, const CAmount& nFees)
 
     int64_t nSubsidy = 0;
     if(nHeight >= 5465) {
-        if((nHeight >= 17000 && dDiff > 75) || nHeight >= 24000) { // GPU/ASIC difficulty calc
+        if((nHeight >= 19000 && dDiff > 75) || nHeight >= 24000) { // GPU/ASIC difficulty calc
             // 2222222/(((x+2600)/9)^2)
             nSubsidy = (2222222.0 / (pow((dDiff+2600.0)/9.0,2.0)));
             if (nSubsidy > 25) nSubsidy = 25;
             if (nSubsidy < 5) nSubsidy = 5;
         } else { // CPU mining calc
-            nSubsidy = (11111.0 / (pow((dDiff+51.0)/6.0,2.0)));
+            nSubsidy = (1111.0 / (pow((dDiff+1.0),2.0)));
             if (nSubsidy > 500) nSubsidy = 500;
-            if (nSubsidy < 25) nSubsidy = 25;
+            if (nSubsidy < 1) nSubsidy = 1;
         }
     } else {
         nSubsidy = (1111.0 / (pow((dDiff+1.0),2.0)));
